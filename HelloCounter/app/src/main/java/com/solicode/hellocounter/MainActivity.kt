c0394fd699b291d7fbec2c5768873350c48f9700
Essachi <essamrachiali@gmail.com>
2025-10-01 10:42:18 +0100

    Basic UI

--- HelloCounter/app/src/main/java/com/solicode/hellocounter/MainActivity.kt
+++ HelloCounter/app/src/main/java/com/solicode/hellocounter/MainActivity.kt
@@ -11,39 +11,20 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.solicode.hellocounter.ui.MainScreen
 
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { App() }
+        setContent { MaterialTheme { MainScreen() } }
     }
 }
 
+@Preview(showBackground = true, widthDp = 360)
 @Composable
-fun App() {
-    MaterialTheme {
-        Greeting()
-    }
+fun PreviewMainScreen() {
+    MaterialTheme { MainScreen() }
 }
 
-@Composable
-fun Greeting() {
-    var count by remember { mutableStateOf(0) }
-
-    // Box = conteneur qui permet d'aligner son contenu
-    Box(
-        modifier = Modifier.fillMaxSize(),              // occupe tout l'écran
-        contentAlignment = Alignment.Center             // centre le contenu
-    ) {
-        Button(onClick = { count++ }) {
-            Text("Hello, count = $count")
-        }
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun PreviewApp() {
-    App()
-}
